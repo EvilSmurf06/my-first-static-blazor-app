d9948ccf1d89f7c88a3b5e6a75dcb74b69274596
EvilSmurf06 <fatih.emecen@ozu.edu.tr>
2022-11-28 18:03:01 +0300

    Multi Language and design

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using AKSoftware.Localization.MultiLanguages;
+using System.Reflection;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddLanguageContainer(Assembly.GetExecutingAssembly());
+builder.Services.AddWMBSC();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
