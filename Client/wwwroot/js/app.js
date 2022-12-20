24dac9025568afcb50db07415e2b5c4d2ad87fc1
EvilSmurf06 <fatih.emecen@ozu.edu.tr>
2022-12-20 16:30:53 +0300

    Some properties added.

--- Client/wwwroot/js/app.js
+++ Client/wwwroot/js/app.js
@@ -1,55 +1,8 @@
-/* Theme Name: Kucra - Responsive Landing page template
-  File Description: Main JS file of the template
-*/
-
-//
-/********************* Menu sticky js ************************/
-//
-
-function windowScroll() {
-  const navbar = document.getElementById("navbar");
-  if (
-    document.body.scrollTop >= 50 ||
-    document.documentElement.scrollTop >= 50
-  ) {
-    navbar.classList.add("nav-sticky");
-  } else {
-    navbar.classList.remove("nav-sticky");
-  }
-}
-window.addEventListener("scroll", (ev) => {
-  ev.preventDefault();
-  windowScroll();
-});
-
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-//
-/********************* Swicher js ************************/
-//
-function toggleSwitcher() {
-    var i = document.getElementById("style-switcher");
-    if (i.style.left === "-189px") {
-        i.style.left = "-0px";
-    } else {
-        i.style.left = "-189px";
-    }
-}
-
-
-function setColor(theme) {
-  document.getElementById("color-opt").href = "./css/colors/" + theme + ".css";
-  toggleSwitcher(false);
-}
 
 //
 /********************* Page Load js ************************/
-//
+
+
 
     function BlazorScrollToId(id) {
         const element = document.getElementById(id);
@@ -60,4 +13,13 @@ function setColor(theme) {
             inline: "nearest"
         });
         }
+}
+
+function showError(msg) {
+    swal({
+        title: "UYARI",
+        text: msg,
+        icon: "error",
+        button: "Tamam"
+    });
 }
